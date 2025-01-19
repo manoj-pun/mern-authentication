88408b2b8c2c2871798aee73ffad5712e27081b1
Manoj Pun <manojpun840@gmail.com>
2025-01-19 18:06:40 +0545

    connected database

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDb from "./config/mongodb.js";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+connectDb();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/",(req,res) => {
+    res.send("Hello World")
+}) 
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`)
+})
