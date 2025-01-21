69fa806c76cb0fd7c315bbbcc5f412992a31b34a
Manoj Pun <manojpun840@gmail.com>
2025-01-21 19:43:36 +0545

    added registe login and logout

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDb from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 connectDb();
 const app = express();
@@ -12,9 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+//API endpoints
 app.get("/",(req,res) => {
     res.send("Hello World")
 }) 
+app.use("/api/auth",authRouter)
 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`)
