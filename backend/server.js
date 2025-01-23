941883cea9fa5f4d51a2bef7eda1293655cddcc2
Manoj Pun <manojpun840@gmail.com>
2025-01-23 19:20:06 +0545

    added getUserData

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDb from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 connectDb();
 const app = express();
@@ -18,6 +19,7 @@ app.get("/",(req,res) => {
     res.send("Hello World")
 }) 
 app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`)
