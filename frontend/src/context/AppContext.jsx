a0c2008b4b9395fa073f46149c2aefcc2a012463
Manoj Pun <manojpun840@gmail.com>
2025-01-25 07:41:40 +0545

    user can login with connected api

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,11 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
 
-    const value = {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [isLoggedin,setIsLoggedin] = useState(false);
+    const [userData,setUserData] = useState(false);
 
+    const getUserData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl+"/api/user/data");
+            console.log(data);
+            data.success ? setUserData(data.userData) : toast.error(data.message);
+        } catch (error) {
+            toast.error(data.message)
+        }
+    }
+
+    const value = {
+        backendUrl,
+        isLoggedin,setIsLoggedin,
+        userData,setUserData,
+        getUserData
     }
 
     return (
